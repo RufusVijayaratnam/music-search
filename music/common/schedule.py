0265ada4b67e834b62821f524e702307e44d652a
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2026-03-21 20:53:40 +0000

    schedule fixes

--- music/common/schedule.py
+++ music/common/schedule.py
@@ -1,21 +1,22 @@
 from abc import ABC, abstractmethod
-from pydantic import BaseModel
+from pydantic import BaseModel, PrivateAttr
 
-class Schedule(ABC, BaseModel):
-    _curr: float
-    _step: int = 0
+class Schedule(BaseModel, ABC):
+    start_value: float
+    curr_step: int = 0
+    _curr: float = PrivateAttr()
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self._curr = self.start_value
 
     @abstractmethod
-    def step(self):
-        self._step += 1
-        pass
+    def step(self) -> None:
+        self.curr_step += 1
 
     def get_value(self) -> float:
         return self._curr
 
 class ConstantSchedule(Schedule):
-    def __init__(self, value: float):
-        self._curr = value
-
-    def step(self):
+    def step(self) -> None:
         super().step()
