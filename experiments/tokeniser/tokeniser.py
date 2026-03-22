a7c5d637fc7541d6f2dd095b8fdab56418405a54
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2026-03-22 14:00:28 +0000

    argparse and train delg

--- experiments/tokeniser/tokeniser.py
+++ experiments/tokeniser/tokeniser.py
@@ -1,16 +1,25 @@
+from enum import Enum
+from music.common.utils.logging import MlFlowLogger
 from music.tokeniser.train import train_tokeniser
 from experiments.tokeniser.configs import experiments
-from music.common.utils.argparse import create_parser, add_exp_arg
+from music.common.utils.argparse import add_mode_arg, create_parser, add_exp_arg
 
+class TokeniserMode(Enum):
+    TRAIN = "train"
 
 def main():
     exps = experiments()
     parser = create_parser("Tokeniser")
+    parser = add_mode_arg(parser, TokeniserMode)
     parser = add_exp_arg(parser, exps)
     args = parser.parse_args()
     exp = args.exp
+    mode = TokeniserMode(args.mode)
     hp = exps[exp]()
-    train_tokeniser(hp)
+    match mode:
+        case TokeniserMode.TRAIN:
+            logger = MlFlowLogger(exp)
+            train_tokeniser(hp, logger)
 
 if __name__ == "__main__":
     main()
--- music/common/utils/argparse.py
+++ music/common/utils/argparse.py
@@ -1,9 +1,17 @@
 import argparse
 from argparse import ArgumentParser
+from enum import Enum
+from typing import Type
 
 def create_parser(program: str | None = None) -> ArgumentParser:
     parser = argparse.ArgumentParser(prog=program)
     return parser
+
+def add_mode_arg(parser: ArgumentParser, choices_enum: Type[Enum]) -> ArgumentParser:
+    modes = [c.value for c in choices_enum]
+    parser.add_argument("mode", choices=modes)
+    return parser
+    
 
 def add_exp_arg(parser: ArgumentParser, exps) -> ArgumentParser:
     parser.add_argument("--exp", choices=list(exps.keys()), required=True)
